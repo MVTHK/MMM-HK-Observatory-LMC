163efd7640fedf63251086162b0db1a16b798460
DESKTOP-LC5ULT0\zh398 <zh398981121@outlook.com>
2022-09-23 01:46:03 +0800

    Added wrapper function

--- MMM-HK-Observatory.js
+++ MMM-HK-Observatory.js
@@ -1,27 +1,61 @@
-WeatherProvider.register("weatherhk", {
-    providerName: "WeatherHK",
+Module.register("weatherhk", {
 
+	weatherProvider: "HKO",
       // Set the default config properties that is specific to this provider
-	  defaults: {
-      weatherProvider: "HKO",
-      reloadInterval: 1 * 60 * 1000, //every 1 minute
-      updateInterval: 30 * 1000, // every 30 seconds
-      timeFormat: config.timeFormat,
-      initialLoadDelay: 0,
-	  },
+	defaults: {
+		header: "MMM-HK-Observatory",
+		reloadInterval: 1 * 60 * 1000, //every 1 minute
+		updateInterval: 30 * 1000, // every 30 seconds
+		timeFormat: config.timeFormat,
+		initialLoadDelay: 0,
+		lang: "en"
+	},
 
-    fetchCurrentWeather() {
-      this.fetchData(this.getUrl())
-			  .then((data) => {
+	dataType: {
+		localWeatherForecast: "flw",
+		nineDayWeatherForecast: "fnd",
+		currentWeatherReport: "rhrread",
+		weatherWarningSummary: "warnsum",
+		weatherWarningInformation: "warningInfo",
+		specialWeatherTips: "swt"
+	},
 
-        })
-        .catch(function (request) {
-          Log.error("Could not load data ... ", request);
-        })
-        .finally(() => this.updateAvailable());
+	lang: {
+		english: "en",
+		traditionalChinese: "tc",
+		simplifiedChinese: "sc"
+	},
+
+	start: function() {
+		Log.info("Start module: " + this.name);
+		this.loaded = false;
+        this.fetchedData = null;
+
+        this.sendSocketNotification("SET_CONFIG", this.config);
+	},
+
+	getStyles: function() {
+		return["MMM-HK-Observatory.css", "font-awesome.css"];
+	},
+
+	getHeader: function() {
+        if (!this.loaded || this.error) {
+            return this.config.header;
+        } else {
+            return this.config.header + " " + this.fetchedData.updateTime;
+        }
     },
 
-    fetchWeatherForecast() {
+	getDomL: function () {
+		const self = this;
+        const wrapper = document.createElement("div");
+        wrapper.className = "MMM-HKO";
+        wrapper.id = "wrapper";
+		wrapper.innerHTML = this.fetchedData.generalSituation;
+		console.log(this.fetchedData.generalSituation)
+		return wrapper;
+	},
 
-    },
-  });
+
+
+});
