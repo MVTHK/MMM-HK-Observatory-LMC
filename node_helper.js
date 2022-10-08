4ffd8284352d680544dd4a915ff27053df6fbcd6
James Zhang <zh398981121@outlook.com>
2022-10-08 18:10:59 +0800

    Correct spelling and format

--- node_helper.js
+++ node_helper.js
@@ -3,43 +3,43 @@ const request = require("request");
 const Log = require("../../js/logger.js");
 
 module.exports = NodeHelper.create({
-  start: function () {
-    Log.log("Starting node helper for: " + this.name);
-    this.config = null;
-  },
+    start: function () {
+        Log.log("Starting node helper for: " + this.name);
+        this.config = null;
+    },
 
-  socketNotificationReceived: function (notification, payload) {
-  if (notification === "SET_CONFIG") {
-    this.config = payload;
-  }
-
-  this.getData();
-  },
-
-  getData: function () {
-  const self = this;
-  Log.info(this.name + ": Fetching data for Hong Kong Observatory");
-
-  const nineDayWeatherForecastURL = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en";
-
-  request(nineDayWeatherForecastURL, function (error, response, body) {
-    if (error || response.statusCode !== 200) {
-      Log.debug(this.name + " :Error getting 9-day Weather Forecast(" + response.statusCode + ")");
-      self.sendSocketNotification("ERROR", response.statusCode);
-      return;
+    socketNotificationReceived: function (notification, payload) {
+    if (notification === "SET_CONFIG") {
+        this.config = payload;
     }
 
-    const generalSituation = JSON.parse(body).generalSituation;
-    const weatherForecast = JSON.parse(body).weatherForecast;
-    const updateTime = JSON.parse(body).updateTime;
+    this.getData();
+    },
 
-    self.sendSocketNotification("DATA", {
-    generalSituation: generalSituation,
-    weatherForecast: weatherForecast,
-    updateTime: updateTime
+    getData: function () {
+    const self = this;
+    Log.info(this.name + ": Fetching data for Hong Kong Observatory");
+
+    const nineDayWeatherForecastURL = "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en";
+
+    request(nineDayWeatherForecastURL, function (error, response, body) {
+        if (error || response.statusCode !== 200) {
+            Log.debug(this.name + " :Error getting 9-day Weather Forecast(" + response.statusCode + ")");
+            self.sendSocketNotification("ERROR", response.statusCode);
+            return;
+        }
+
+        const generalSituation = JSON.parse(body).generalSituation;
+        const weatherForecast = JSON.parse(body).weatherForecast;
+        const updateTime = JSON.parse(body).updateTime;
+
+        self.sendSocketNotification("DATA", {
+        generalSituation: generalSituation,
+        weatherForecast: weatherForecast,
+        updateTime: updateTime
+        });
     });
-  });
 
-  setTimeout(this.getData.bind(this), this.config.updateInterval);
-  }
+    setTimeout(this.getData.bind(this), this.config.updateInterval);
+    }
 });
